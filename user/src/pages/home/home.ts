e4bc13c14d483e166e735e545d9b8ea84a5b68f8
adielre <adiel46@gmail.com>
2018-05-02 17:08:10 +0300

    bind screens

--- user/src/pages/home/home.ts
+++ user/src/pages/home/home.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Page2Page } from '../page2/page2';
 import { AlertController } from 'ionic-angular';
+
 
 @Component({
   selector: 'page-home',
@@ -9,24 +10,44 @@ import { AlertController } from 'ionic-angular';
 })
 export class HomePage {
 
+  @Input() location : string
+    
   constructor(public navCtrl: NavController, private alertCtrl: AlertController) {
-
+    
   }
-
+  
   onLoadPlaces()
   {
-    
-      this.presentAlert();
-    
-      this.navCtrl.push(Page2Page);
+      if(this.location == undefined)
+      {
+        this.presentAlert();
+        //console.log("hello");
+      } 
+      else
+      {
+       
+       // this.location = undefined;
+        this.navCtrl.push(Page2Page);
+
+        
+
+        //console.log("world");
+      }
+        
+
+        
   }
 
-  presentAlert() {
+  presentAlert() 
+  {
     let alert = this.alertCtrl.create({
+      
       title: 'שגיאה',
       subTitle: 'אתה חייב להכניס מיקום',
+      cssClass: 'rtlAlert',
       buttons: ['אשר']
     });
     alert.present();
   }
+
 }
